d7e7fd37484cde246d2791b1a9ca765ddf9a4c35
Bhavani0915 <bheemireddy0915@gmail.com>
2026-06-20 17:15:08 +0530

    Initial commit: Grocery Store Management Application

---
+++ backend/sql_connection.py
@@ -0,0 +1,8 @@
+import mysql.connector
+__cnx = None
+def get_sql_connection():
+    print("Opening mysql connection")
+    global __cnx
+    if __cnx is None:
+        __cnx = mysql.connector.connect(user = 'root', password = 'Sujatha@1984', database='grocery_store')
+    return __cnx
